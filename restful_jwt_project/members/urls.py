395e9f0e975a189454763a446efae2fe3b12edf9
ElvinKim <bonwho09@gmail.com>
2016-12-29 22:46:14 +0900

    [edit] member detail view

--- restful_jwt_project/members/urls.py
+++ restful_jwt_project/members/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls  import url
-from members.views import *
+from members.views import MemberListAPIView
+from members.views import MemberDetailAPIView
 
 
 urlpatterns = [
     url(r'^$', MemberListAPIView.as_view(), name='member_list'),
+    url(r'^(?P<pk>\d+)/$', MemberDetailAPIView.as_view(), name='member_detail'),
 ]
--- restful_jwt_project/members/views.py
+++ restful_jwt_project/members/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
 
 from members.models import Member
 from members.serializers import MemberSerializer
 
 
 class MemberListAPIView(ListAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+class MemberDetailAPIView(RetrieveAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
